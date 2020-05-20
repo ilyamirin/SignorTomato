3823b41581e364e7e619c2fe0845526729f32ac0
ilyamirin <ilya.mirin@gmail.com>
2020-05-20 22:35:07 +1000

    add ogg tmp

--- converter.sh
+++ converter.sh
@@ -3,8 +3,10 @@ echo "Press [CTRL+C] to stop.."
 while true
 do
 	for filename in ./*.wav; do
-        oggenc -o "$filename.ogg" $filename
+        oggenc -o "$filename.ogg.tmp" $filename
+        mv "$filename.ogg.tmp" "$filename.ogg"
         rm $filename
     done
+    sleep 1
 done
  
